8e0ea9e32559f4293a9bd6d808fdc6c50e2a8399
Tom Sinclair <tsinclair552@gmail.com>
2020-07-12 11:24:42 -0600

    First commit for remote git tutorial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace remote_git_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
